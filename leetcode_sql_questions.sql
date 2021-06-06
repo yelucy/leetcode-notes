5abb400702bdcd56e0f11f7d88d944cb6bd0010a
Lucy <lucy.x.ye@gmail.com>
2021-06-06 00:09:38 -0400

    sql 6/5

--- leetcode_sql_questions.sql
+++ leetcode_sql_questions.sql
@@ -159,6 +159,35 @@ where xrank = 1
 order by 1; 
 
 
+######## 1083. Sales Analysis II ######## [EASY]
+
+-- Write an SQL query that reports the buyers who have bought S8 but not iPhone. 
+-- Note that S8 and iPhone are products present in the Product table.
+
+# Schema: 
+drop table if exists Product;
+drop table if exists Sales; 
+Create table If Not Exists Product (product_id int, product_name varchar(10), unit_price int);
+Create table If Not Exists Sales (seller_id int, product_id int, buyer_id int, sale_date date, quantity int, price int);
+insert into Product (product_id, product_name, unit_price) values ('1', 'S8', '1000');
+insert into Product (product_id, product_name, unit_price) values ('2', 'G4', '800');
+insert into Product (product_id, product_name, unit_price) values ('3', 'iPhone', '1400');
+insert into Sales (seller_id, product_id, buyer_id, sale_date, quantity, price) values ('1', '1', '1', '2019-01-21', '2', '2000');
+insert into Sales (seller_id, product_id, buyer_id, sale_date, quantity, price) values ('1', '2', '2', '2019-02-17', '1', '800');
+insert into Sales (seller_id, product_id, buyer_id, sale_date, quantity, price) values ('2', '1', '3', '2019-06-02', '1', '800');
+insert into Sales (seller_id, product_id, buyer_id, sale_date, quantity, price) values ('3', '3', '3', '2019-05-13', '2', '2800');
+select * from Product;
+select * from Sales; 
+
+# Solution:
+select s.buyer_id 
+from Sales s 
+inner join Product p on s.product_id = p.product_id 
+group by 1 
+having sum(case when p.product_name = 'S8' then 1 else 0 end) > 0 
+and sum(case when p.product_name = 'iPhone' then 1 else 0 end) = 0; 
+
+
 ######## 1113. Reported Posts ######## [EASY]
 
 -- Write an SQL query that reports the number of posts reported yesterday for each report reason. Assume today is 2019-07-05. 
